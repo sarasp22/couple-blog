d8df1e9a023b3fa4d1d30031b1235a55ce84a5a3
Spadari Sara <saraspadari@yahoo.com>
2026-09-16 14:49:31 +0200

    pin france 3

--- assets/js/viaggi.js
+++ assets/js/viaggi.js
@@ -66,7 +66,7 @@ const DESTINATIONS = [
     id: 'francia',
     label_it: 'Francia',
     label_fr: 'France',
-    pin: { x: 48, y: 30 },
+    pin: { x: 49, y: 30 },
     col: 'left',
     places: [
       {
